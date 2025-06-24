f7730933ff95bd0227c898dd50a9be64f91d38d5
Adeamanah <Adeamanah.710@student.uns.ac.id>
2025-06-24 22:18:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from produk import RotiManis, Croissant, ButterCookies, Muffin
+# main.py
+from proses import *
 
-produk_list = []
+def menu():
+    while True:
+        print("\n====== HANARI BAKERY SYSTEM ======")
+        print("1. Tambah Produk")
+        print("2. Tampilkan Semua Produk")
+        print("3. Kalkulator Estimasi Profit")
+        print("4. Simulasi Produksi")
+        print("0. Keluar")
 
-def menu(): ...
-def tambah_produk(): ...
-def tampilkan_produk(): ...
-def kalkulator_profit(): ...
-def simulasi_produksi(): ...
+        pilih = input("Pilih menu: ")
+        if pilih == "1": tambah_produk()
+        elif pilih == "2": tampilkan_produk()
+        elif pilih == "3": kalkulator_profit()
+        elif pilih == "4": simulasi_produksi()
+        elif pilih == "0": print("Terima kasih!"); break
+        else: print("Pilihan tidak valid.")
 
 if __name__ == "__main__":
     menu()
